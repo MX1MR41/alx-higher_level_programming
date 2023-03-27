123527a05bb79a346b7fa35b96d99d0ddb014988
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2023-03-27 13:25:02 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" 
-Module that adds all arguments to a Python list, and then
+""" Module that adds all arguments to a Python list, and then
 save them to a file
 """
+
 import sys
 import os.path
 
